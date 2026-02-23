2bfcd765d696842a241e69974a032d1ea2138b32
axl2354 <89017810+axl2354@users.noreply.github.com>
2026-02-24 00:00:23 +0700

    main menu
    literally just the main menu

---
+++ jokerpokerchokerstroker/RunSession.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+#define STB_IMAGE_IMPLEMENTATION
+#include <vector>
+#include <algorithm>
+
+void game(){
+    
+}
+
+int main(int argc, char **argv)
+{
+    int input;
+    cout<<"Joker Poker Choker Sroker"<<endl;
+    cout<<"1-start"<<endl;
+    cout<<"0-end"<<endl<<endl;
+    cout<<"Enter number: ";
+    cin>>input;
+
+    if (input==1)
+    {
+        game();
+    }else{
+        cout<<"okay bye";
+    }
+    
+}
